b94270a7a2c3a6c931646a0d034e41fed1e1a925
Ernesto <emagodev@gmail.com>
2025-01-03 11:07:48 +0100

    functions to prepare ranking charts

---
+++ server/data_transformations.R
@@ -0,0 +1,57 @@
+library(data.table)
+
+# Función para obtener el ranking de provincias
+get_ranking_provinces <- function(df_prov, province_list, min_year = 2019, max_year = 2023) {
+    df_prov <- as.data.table(df_prov)
+    all_ranks <- lapply(min_year:max_year, function(year) {
+        df_prov[AÑO == year & PROVINCIA_DESTINO %in% province_list & PROVINCIA_DESTINO != "Total",
+            .(TURISTAS = sum(TURISTAS)),
+            by = PROVINCIA_DESTINO
+        ][order(-TURISTAS), PROVINCIA_DESTINO]
+    })
+
+    all_ranks_df <- as.data.table(do.call(cbind, all_ranks))
+    setnames(all_ranks_df, as.character(min_year:max_year))
+    return(all_ranks_df)
+}
+
+# Función para obtener el ranking en formato para Tableau
+get_tableau_ranking <- function(df_prov, df_ranking_provinces, min_year = 2019, max_year = 2023) {
+    df_prov <- as.data.table(df_prov)
+    all_rank_year <- unique(df_prov[, .(PROVINCIA_DESTINO)])
+    distintct_provinces <- df_ranking_provinces[[1]]
+
+    all_rank_year <- all_rank_year[PROVINCIA_DESTINO %in% distintct_provinces]
+
+    for (year in min_year:max_year) {
+        rank_df_year <- data.table(
+            PROVINCIA_DESTINO = df_ranking_provinces[[year]],
+            RANK = 1:nrow(df_ranking_provinces)
+        )
+        setnames(rank_df_year, "RANK", paste0("RANK_", year))
+
+        all_rank_year <- merge(all_rank_year, rank_df_year, by = "PROVINCIA_DESTINO", all.x = TRUE)
+    }
+
+    all_rank_year <- all_rank_year[order(get(paste0("RANK_", min_year)))]
+    return(all_rank_year)
+}
+
+# Función para obtener datos para un bump chart
+get_bumpchart_ranking <- function(df_ranking_tableau) {
+    df_ranking_tableau <- as.data.table(df_ranking_tableau)
+    n_elements <- nrow(df_ranking_tableau)
+
+    bump_format <- melt(df_ranking_tableau,
+        id.vars = "PROVINCIA_DESTINO",
+        measure.vars = patterns("^RANK_"),
+        variable.name = "Year",
+        value.name = "Position"
+    )
+
+    bump_format[, Year := as.numeric(sub("RANK_", "", Year))]
+    bump_format[, Position := n_elements - Position]
+    setnames(bump_format, "PROVINCIA_DESTINO", "Province")
+
+    return(bump_format)
+}
