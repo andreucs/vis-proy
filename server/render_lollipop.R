980a127874efe8ef9c89c373f53cb095f42954b5
LaiqianDS <ji.laiqian@gmail.com>
2025-01-09 18:35:22 +0100

    add lolli

--- server/render_lollipop.R
+++ server/render_lollipop.R
@@ -1,26 +1,27 @@
 render_lollipop <- function(output, input, ocup1, ccaa) {
   
   output$lollipop <- renderPlotly({
+    ccaa$Comunidad_autonoma <- gsub("^Región de", "", ccaa$Comunidad_autonoma)
+    ccaa$Comunidad_autonoma <- gsub("^Comunidad de", "", ccaa$Comunidad_autonoma)
+    ccaa$Comunidad_autonoma <- gsub("^Comunidad", "C.", ccaa$Comunidad_autonoma)
     
     ocup1 <- merge(ocup1, ccaa, by.x="PROVINCIA", by.y="Provincia") |> filter(LUGAR_RESIDENCIA != "Total")
     ocup1$anyomes <- paste(ocup1$AÑO, ocup1$MES, sep = "-")
     
     ocup1 <- ocup1 |>
       filter(AÑO > 2010) |>
-      group_by(AÑO, MES, LUGAR_RESIDENCIA, Comunidad_autonoma) |>
-      summarise(total_pernoctaciones = sum(PERNOCTACIONES, na.rm = TRUE), .groups = "drop") |>
       pivot_wider(
         names_from = LUGAR_RESIDENCIA,
-        values_from = total_pernoctaciones,
-        names_prefix = "pernoctaciones_"
+        values_from = VIAJEROS,
+        names_prefix = "viajeros_"
       )
     
     d <- ocup1 |> 
       group_by(Comunidad_autonoma) |>
       summarise(mean_residentes = 
-                  mean(`pernoctaciones_Residentes en España`, 
+                  mean(`viajeros_Residentes en España`, 
                        na.rm = TRUE), 
-                mean_extranjeros = mean(`pernoctaciones_Residentes en el Extranjero`, 
+                mean_extranjeros = mean(`viajeros_Residentes en el Extranjero`, 
                                         na.rm = TRUE), .groups = "drop")
     
     d <- d[rev(order(d$mean_extranjeros)), ]
@@ -35,6 +36,13 @@ render_lollipop <- function(output, input, ocup1, ccaa) {
         title = "Gender earnings disparity",
         xaxis = list(title = "Annual Salary (in thousands)"),
         margin = list(l = 65)
+      ) |>
+      config(
+        modeBarButtonsToRemove = c(
+          "zoom2d", "pan2d", "select2d", "lasso2d", 
+          "zoomIn2d", "zoomOut2d", "autoScale2d", 
+          "hoverClosestCartesian", "hoverCompareCartesian"
+        )
       )
     
   })
